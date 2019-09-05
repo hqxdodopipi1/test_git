4b2c9454d322549b93d266e678b00c98c5f2727d
Morris <100992036@qq.com>
2019-09-05 20:28:30 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: Morris
+	> Mail: 100992036@qq.com
+	> Created Time: 四  9/ 5 20:28:01 2019
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[]) 
+{
+    return 0; 
+}
